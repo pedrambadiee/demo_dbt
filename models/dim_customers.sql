0c7c83bc8b8dff10db2c2608be0c9d10baf60c92
Pedram Badiee <pedbadiee@pedrams-air.localdomain>
2021-11-26 11:45:32 +0100

    stg models and ref function added in dim_customers

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -3,22 +3,13 @@
 )}}
 
 with customers as (
-	select
-	id as customer_id,
-	first_name,
-	last_name
-	from raw.raw_customers
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-	select 
-	id as order_id,
-	user_id as customer_id,
-	order_date,
-	status
-	
-	from raw.raw_orders
+    select * from {{ ref('stg_orders') }}
 ),
+
 
 customer_orders as (
 	
--- target/run/demo_dbt/models/dim_customers.sql
+++ target/run/demo_dbt/models/dim_customers.sql
@@ -5,22 +5,13 @@
     
 
 with customers as (
-	select
-	id as customer_id,
-	first_name,
-	last_name
-	from raw.raw_customers
+    select * from "demo_dbt"."public"."stg_customers"
 ),
 
 orders as (
-	select 
-	id as order_id,
-	user_id as customer_id,
-	order_date,
-	status
-	
-	from raw.raw_orders
+    select * from "demo_dbt"."public"."stg_orders"
 ),
+
 
 customer_orders as (
 	
